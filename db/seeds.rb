840ed9d27c45eb4df4bdecf652b42911ba967a84
katherine <muedasnka@gmail.com>
2014-09-13 16:05:34 -0600

    add seed for cats

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,23 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
-#
-# Examples:
-#
-#   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
-#   Mayor.create(name: 'Emanuel', city: cities.first)
+user = User.find_or_create_by!(email: "example@example.com") do |user|
+ user.password= '12345678'
+ user.password_confirmation= '12345678'
+end
+puts "created #{user.email}"
+ 
+25.times do
+  photo = open("http://placekitten.com/g/#{Random.rand(300..500)}/#{Random.rand(300..500)}")
+  photo = user.pictures.create(title: 'Awesome Photo', description: 'Awesome Description', photo: photo, category: 'cat')
+  puts "created photo for #{user.email}"
+end
+ 
+user = User.find_or_create_by!(email: "example2@example.com") do |user|
+ user.password= '12345678'
+ user.password_confirmation= '12345678'
+end
+ 
+puts "created #{user.email}"
+25.times do
+  photo = open("http://placekitten.com/g/#{Random.rand(300..500)}/#{Random.rand(300..500)}")
+  photo = user.pictures.create(title: 'Awesome Photo', description: 'Awesome Description', photo: photo, category: 'cat')
+  puts "created photo for #{user.email}"
+end
