c9106506d4f69a012a0950c13e3cd39afcbf1df2
Marvin Santizo <santizomarvin88@gmail.com>
2014-09-13 13:22:53 -0600

    modified routes and comment controller

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -17,6 +17,7 @@ class CommentsController < ApplicationController
   private
 
   def comment_params
+    params.require(:comment).permit(:body, :picture_id)
   end
 
   def find_photo
--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,2 +1,6 @@
 class Comment < ActiveRecord::Base
+	belongs_to :picture
+	belongs_to :user
+
+	validate :body, presence: true
 end
