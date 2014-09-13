d2326d8a7d46c330a7c9ae0b53a251707f0d0edb
Marvin Santizo <santizomarvin88@gmail.com>
2014-09-13 10:43:37 -0600

    created controller for users, and created views page for users, and changed routes and added root

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,11 @@
 Rails.application.routes.draw do
+
   devise_for :users
+
+  resources :pictures do
+    resources :comments
+  end
+root 'picture#index'
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
